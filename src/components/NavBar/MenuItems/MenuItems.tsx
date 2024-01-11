8ce40ec4262e910f5fe3bc10e6ee81ef43e1fbb6
Tony5300 <tony_tolen@ziggo.nl>
2024-01-11 17:21:51 +0100

    first commit

---
+++ src/components/NavBar/MenuItems/MenuItems.tsx
@@ -0,0 +1,37 @@
+import { MdOutlineClose } from "react-icons/md";
+
+type NavBarProps = {
+  handleShowMenu: () => void;
+};
+
+const MenuItems: React.FC<NavBarProps> = ({ handleShowMenu }) => {
+  return (
+    <>
+      <button
+        className="absolute left-3 top-2 z-10 appearance-none"
+        onClick={handleShowMenu}
+      >
+        <MdOutlineClose className="cursor-pointer" />
+      </button>
+      <ul className="menu bg-base-200 w-56 absolute top-0 h-screen">
+        <li>
+          <a>Collections</a>
+        </li>
+        <li>
+          <a>Men</a>
+        </li>
+        <li>
+          <a>Women</a>
+        </li>
+        <li>
+          <a>About</a>
+        </li>
+        <li>
+          <a>Contact</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default MenuItems;
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import profileImg from "../../assets/images/image-avatar.png";
+import MenuItems from "./MenuItems/MenuItems";
+
+const NavBar = () => {
+  const [menu, setMenu] = useState(false);
+
+  const handleShowMenu = () => {
+    setMenu(!menu);
+  };
+  return (
+    <header>
+      <nav>
+        <div className="navbar bg-base-100 md:hidden">
+          <div className="flex-none">
+            <button
+              className="btn btn-square btn-ghost"
+              onClick={handleShowMenu}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          {menu ? <MenuItems handleShowMenu={handleShowMenu} /> : null}
+
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl">sneakers</a>
+          </div>
+          <div className="flex-none">
+            <img src={profileImg} alt="profile" className="w-12" />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
