5de5a4bbf9c08b35ddd26507718bcee2b8a92f7e
Tony5300 <tony_tolen@ziggo.nl>
2024-01-12 17:27:25 +0100

    bugfix: fixed a bug about the caroussel. Now it is working.

--- src/components/ImageCarrousel/ImageCarrousel.tsx
+++ src/components/ImageCarrousel/ImageCarrousel.tsx
@@ -3,22 +3,17 @@ import imageData from "../../data/ImageData.json";
 import nextIcon from "../../assets/images/icon-next.svg";
 import prevIcon from "../../assets/images/icon-previous.svg";
 
-type ImageDataType = {
-  id: number;
-  image: string;
-};
-
-const images: ImageDataType[] = imageData;
-
 const ImageCarrousel = () => {
-  const [nextSlide, setNextSlide] = useState(0);
-  const imgArray = nextSlide < images.length - 1;
+  const [nextSlide, setNextSlide] = useState(1);
+  const isLastSlide = nextSlide <= imageData.length - 1;
 
   const handleNextSlide = () => {
-    if (imgArray) {
+    console.log(`imageArray:`, isLastSlide);
+
+    if (isLastSlide) {
       setNextSlide(nextSlide + 1);
     } else {
-      setNextSlide(0);
+      setNextSlide(1);
     }
 
     console.log(`you clicked on the next btn`, nextSlide);
@@ -28,14 +23,16 @@ const ImageCarrousel = () => {
   return (
     <section>
       <h3>
-        ({nextSlide + 1} of {imageData.length})
+        ({nextSlide} of {imageData.length})
       </h3>
       <div className="carousel h-[23rem]">
-        {images.map((image) => (
-          <div className="carousel-item w-full" key={image.id}>
-            <img src={image.image} className="w-full" alt="product-photo" />
-          </div>
-        ))}
+        {imageData.map((image) =>
+          nextSlide === image.id ? (
+            <div className="carousel-item w-full" key={image.id}>
+              <img src={image.image} className="w-full" alt="product-photo" />
+            </div>
+          ) : null
+        )}
       </div>
       <div className="button-container">
         <button className="image-btn">
